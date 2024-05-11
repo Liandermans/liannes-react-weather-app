333fb666074211bba67e4234517a7a1ff83c58a4
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-05-11 13:23:45 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Open-source code on{" "}
-        <a
-          href="https://github.com/Liandermans/liannes-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-        , hosted on{" "}
-        <a
-          href="https://liannes-react-weather-app.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Open-source code on{" "}
+          <a
+            href="https://github.com/Liandermans/liannes-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          , hosted on{" "}
+          <a
+            href="https://liannes-react-weather-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

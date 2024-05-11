b80afd9222894bf5f19368bcbc7942b8df55cb48
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-05-11 15:07:19 +0200

    Added CSS

--- src/Weather.js
+++ src/Weather.js
@@ -8,27 +8,34 @@ import { faWind } from "@fortawesome/free-solid-svg-icons";
 export default function Weather() {
   return (
     <div className="Weather">
-      <ul>
-        <li>New York</li>
-        <li>Saturday, 13:40</li>
+      <ul className="city-date">
+        <li>
+          <h2>New York</h2>
+        </li>
+        <li className="date-time">Saturday, 13:40</li>
       </ul>
+
       <form>
         <input type="text" placeholder="Enter city" autoFocus="on" />
-        <input type="submit" value="Search" />
+        <input type="submit" value="Search" />{" "}
+        <FontAwesomeIcon icon={faLocationDot} className="location-icon" />
       </form>
-      <FontAwesomeIcon icon={faLocationDot} />
+
       <ul className="weather-today">
         <li>Icon</li>
         <li>Temperature current</li>
         <li>Description</li>
       </ul>
+
       <div className="wrapper">
         <div>
-          <FontAwesomeIcon icon={faDroplet} /> <br />
+          <FontAwesomeIcon icon={faDroplet} className="icon" />
+          <br />
           Humidity
         </div>
         <div>
-          <FontAwesomeIcon icon={faWind} /> <br />
+          <FontAwesomeIcon icon={faWind} className="icon" />
+          <br />
           Wind
         </div>
       </div>

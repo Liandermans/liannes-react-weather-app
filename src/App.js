69f37b409e7396d97f97a6d8f00f466506866b8e
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-05-11 13:02:36 +0200

    Basic project setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Open-source code on{" "}
+        <a
+          href="https://github.com/Liandermans/liannes-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+        , hosted on{" "}
+        <a
+          href="https://liannes-react-weather-app.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
